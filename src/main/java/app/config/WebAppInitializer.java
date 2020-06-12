8f697d4320d93eec11f099cbf384af9d11dd4399
bstacksp <vovvg777@gmail.com>
2020-06-12 17:11:24 +0300

    Mvc home page working

--- src/main/java/app/config/WebAppInitializer.java
+++ src/main/java/app/config/WebAppInitializer.java
@@ -2,19 +2,19 @@ package app.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-//public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
-//	@Override
-//	protected Class<?>[] getRootConfigClasses() {
-//		return new Class[]{JpaConfiguration.class};
-//	}
-//
-//	@Override
-//	protected Class<?>[] getServletConfigClasses() {
-//		return new Class[]{WebConfiguration.class};
-//	}
-//
-//	@Override
-//	protected String[] getServletMappings() {
-//		return new String[]{"/"};
-//	}
-//}
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[]{JpaConfiguration.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[]{WebConfiguration.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[]{"/"};
+	}
+}
